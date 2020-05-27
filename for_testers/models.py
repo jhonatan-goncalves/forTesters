90ad1c053f7e4f196ca6ee3befc03c0d2cd1ccf4
jhonatan-goncalves <jhonny_will2@hotmail.com>
2020-05-26 21:15:15 -0300

    Alterações

--- for_testers/models.py
+++ for_testers/models.py
@@ -41,8 +41,6 @@ class Cenario(models.Model):
     resultado_obtido = models.TextField()
     data_criacao = models.DateTimeField(default=timezone.now)
 
-    def __str__(self):
-        return self.nome
 
 class Roteiro(models.Model):
     
@@ -59,7 +57,4 @@ class Roteiro(models.Model):
     projetos = models.ManyToManyField(Projeto)
     cenarios = models.ManyToManyField(Cenario)
     data_criacao = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.nome
  
